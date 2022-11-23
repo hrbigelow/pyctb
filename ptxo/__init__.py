b3c885321c5672655a6ba2f9410e7f48ac317f93
Henry Bigelow <hrbigelow@gmail.com>
2022-11-22 23:41:06 -0800

    modularize

---
+++ ptxo/__init__.py
@@ -0,0 +1,62 @@
+import traceback
+import sys
+import torch
+
+OPS = [ torch.nn.modules.conv.Conv1d ]
+
+def get_forward_obj(frame):
+    """
+    Finds an object with a forward method in the current frame's globals
+    and whose class is in a registry of classes
+    """
+    obj = next((o for o in frame.f_globals.values() if o in OPS), None)
+    if obj is None:
+        return None
+    if obj.forward.__code__ is frame.f_code:
+        return obj
+    return None
+
+def _arg_to_str(val):
+    vtype = type(val)
+    if isinstance(val, torch.Tensor):
+        ten = val
+        shape_str = '[' + ','.join(str(d) for d in ten.shape) + ']'
+        dtype_str = str(ten.dtype).split('.')[1]
+        dev_str = ten.device.type
+        val = f'{shape_str}:{dtype_str}:{dev_str}'
+    elif isinstance(val, (tuple, list, set)):
+        val = vtype(_arg_to_str(v) for v in val)
+    elif isinstance(val, dict):
+        val = vtype({_arg_to_str(k): _arg_to_str(v) for k, v in val.items()})
+    else:
+        pass
+    return val
+
+def _print_tensors(exc_type, exc_value, tb):
+    while tb:
+        frame = tb.tb_frame
+        obj = get_forward_obj(frame)
+        if obj is not None:
+            items = []
+            for arg, val in frame.f_locals.items():
+                if arg == 'self':
+                    continue
+                val = _arg_to_str(val)
+                item = f'{arg}={val}'
+                items.append(item)
+            argstr = ', '.join(items)
+            msg = f'    [ptxo]: {obj.__module__}.{obj.__name__}({argstr})'
+            print(msg, file=sys.stderr)
+        traceback.print_tb(tb, limit=1)
+        tb = tb.tb_next
+
+OLD_HOOK = None
+
+def on(): 
+    # OLD_HOOK = sys.excepthook
+    sys.excepthook = _print_tensors
+
+def off():
+    # sys.excepthook = OLD_HOOK
+    sys.excepthook = sys.__excepthook__
+
--- test.py
+++ test.py
@@ -9,29 +9,6 @@ Difficulty is finding the identity of the caller:
 
 https://stackoverflow.com/questions/2203424/python-how-to-retrieve-class-information-from-a-frame-object
 
-"""
-import traceback
-import sys
-import torch
-
-conv = torch.nn.Conv1d(5, 5, 10)
-
-def b(ace, jar):
-    inp = torch.zeros([5, 100])
-    return conv(inp)
-    # return a(6, {'a', 'b'})
-
-def c(foo, bar):
-    return b('abce', [5,3,1])
-
-def d(yab, tab):
-    return c(5, 'a')
-
-def e(*args):
-    return d({}, 'abc')
-
-OPS = [ torch.nn.modules.conv.Conv1d ]
-
 def get_name(frame):
     code = frame.f_code
     name = code.co_name
@@ -44,75 +21,29 @@ def get_name(frame):
             name = '%s.%s' % ( objname, name )
             break
     return name
+"""
+import torch
+import ptxo
+import sys
 
-def get_forward_obj(frame):
-    """
-    Finds an object with a forward method in the current frame's globals
-    and whose class is in a registry of classes
-    """
-    obj = next((o for o in frame.f_globals.values() if o in OPS), None)
-    if obj is None:
-        return None
-    if obj.forward.__code__ is frame.f_code:
-        return obj
-    return None
+conv = torch.nn.Conv1d(5, 5, 10)
 
-def arg_to_str(val):
-    vtype = type(val)
-    if isinstance(val, torch.Tensor):
-        ten = val
-        shape_str = str(list(ten.shape))
-        dtype_str = str(ten.dtype).split('.')[1]
-        dev_str = ten.device.type
-        val = f'{shape_str}:{dtype_str}:{dev_str}'
-    elif isinstance(val, (tuple, list, set)):
-        val = vtype(arg_to_str(v) for v in val)
-    elif isinstance(val, dict):
-        val = vtype({arg_to_str(k): arg_to_str(v) for k, v in val.items()})
-    else:
-        pass
-    return val
+def b(ace, jar):
+    inp = torch.zeros([5, 100])
+    return conv(inp)
 
-def print_tensors(exc_type, exc_value, tb):
-    while tb:
-        frame = tb.tb_frame
-        obj = get_forward_obj(frame)
-        if obj is not None:
-            items = []
-            for arg, val in frame.f_locals.items():
-                if arg == 'self':
-                    continue
-                val = arg_to_str(val)
-                item = f'{arg}={val}'
-                items.append(item)
-            argstr = ', '.join(items)
-            msg = f'    [PXTO message]: {obj.__module__}.{obj.__name__}({argstr})'
-            print(msg, file=sys.stderr)
-            # print(exc_type, exc_value)
-        traceback.print_tb(tb, limit=1)
-        tb = tb.tb_next
-    # raise exc_value
-
-OLD_HOOK = None
-
-def set_hook(hook):
-    OLD_HOOK = sys.excepthook
-    sys.excepthook = hook
-
-def unset_hook():
-    sys.excepthook = OLD_HOOK
+def c():
+    return b('abce', [5,3,1])
 
 def main():
     use_hook = (sys.argv[1] == 'y')
     if use_hook:
-        set_hook(print_tensors)
-    e()
-    if use_hook:
-        unset_hook()
+        ptxo.on()
+    c()
 
+    if use_hook:
+        ptxo.off()
 
 if __name__ == '__main__':
     main()
-
-
 
