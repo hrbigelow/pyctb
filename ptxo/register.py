7e51209925fd3f343988ae81d4d60cd6d923f1a4
Henry Bigelow <hrbigelow@gmail.com>
2022-11-24 13:41:30 -0800

    add tf tests

--- ptxo/register.py
+++ ptxo/register.py
@@ -17,9 +17,12 @@ def tf():
     Render a tf.Tensor or tf.Variable
     """
     import tensorflow as tf
+    from tensorflow.python.framework.ops import EagerTensor
     fmap = {
+            EagerTensor: rfuncs.tf_tensor,
             tf.Tensor: rfuncs.tf_tensor,
             tf.Variable: rfuncs.tf_tensor 
             }
+
     return fmap
 
