a79e49e02101ee9ad24d0cde5d72496e7c46d9b9
Henry Bigelow <hrbigelow@gmail.com>
2022-11-28 14:35:50 -0800

    packaging progress

--- tests/torch_test.py
+++ tests/torch_test.py
@@ -3,4 +3,12 @@ import torch
 def matmul_test():
     a = torch.randn(3,5,7)
     b = torch.randn(3,5,7)
+    return torch.matmul(a, b)
+
+def binop_test():
+    a = torch.randn(3,5,7)
+    b = torch.randn(3,6,7)
+    return a / b
+
+
 
